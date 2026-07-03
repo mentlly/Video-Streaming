731fcdba1bbf868722a956da0185743f235a5bb1
Samuel Fred <ronaldosandysam1525@gmail.com>
2026-07-04 04:01:38 +0530

    changed and added some config in ffmpeg args

--- backend/go-video-management/main.go
+++ backend/go-video-management/main.go
@@ -129,14 +129,17 @@ func videoProccessor(dir string) {
 
 	args := []string{
 		"-i", "original.mp4", // Input file
+		"-threads", "0", // Max CPU cores
+		"-preset", "ultrafast",
 		"-vcodec", "libx264", // H.264 video codec
+		"-acodec", "copy", // Direct audio copy (no encoding)
 		"-vf", "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2",
 		"-crf", "23", // Compression value
 		"-g", "60",
 		"-keyint_min", "60",
 		"-sc_threshold", "0", // Making a key frame every 60 frames
 		"-f", "hls", // For output to be in hls format
-		"-hls_time", "5", // Making 5 seconds segments
+		"-hls_time", "2", // Making 2 seconds segments
 		"-hls_playlist_type", "event",
 		"-hls_segment_filename", "streaming_output/file%03d.ts",
 		"streaming_output/stream.m3u8",
