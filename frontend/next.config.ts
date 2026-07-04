20f84280a93b52da8474fc46fe8ec25e5de89d55
Samuel Fred <ronaldosandysam1525@gmail.com>
2026-07-04 15:41:15 +0530

    added allowedDevOrigins

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  allowedDevOrigins: ['192.168.1.*'],
 };
 
 export default nextConfig;
