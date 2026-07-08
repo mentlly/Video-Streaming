c0cda19db985c70c94f3c5b4f5b19528be3c5c23
Samuel Fred <ronaldosandysam1525@gmail.com>
2026-07-08 21:34:19 +0530

    Connection to postgres db and creation of video table

--- backend/video-manager/services/db.go
+++ backend/video-manager/services/db.go
@@ -2,13 +2,14 @@ package services
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-var DB *pgxpool.Pool
+var dbpool *pgxpool.Pool
 
 func ConnDb() {
 	//Creating a connection with database
@@ -21,4 +22,22 @@ func ConnDb() {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
 	defer dbpool.Close()
+
+	log.Println("Database connected ....")
+
+	commandTag, err := dbpool.Exec(
+		ctx,
+		`CREATE TABLE IF NOT EXISTS video 
+		(video_id varchar(10) PRIMARY KEY NOT NULL, 
+		title varchar(100) NOT NULL, 
+		description varchar(5000),
+		duration INT(4) NOT NULL, 
+		channel_id varchar(10) NOT NULL,
+		FOREIGN KEY (channel_id) REFERENCES channel(channel_id));`,
+	)
+	if err != nil {
+		log.Printf("Failed to create table: %w\n", err)
+		return
+	}
+	fmt.Printf("Table status: %s\n", commandTag.String())
 }
